7593bdef66f9b2e6cc636734c1b83e1d2803fc94
lorenzosalmina <lorenzo.salmina@epfl.ch>
2020-06-05 21:29:38 +0200

    button test

--- sketch.js
+++ sketch.js
@@ -8,15 +8,6 @@ function preload() {
 }
 
 function setup() {
-  noLoop();
-}
-
-function draw() {
-  background(200);
-  // Get the magnitude and name of the earthquake out of the loaded JSON
-  let earthquakeMag = data.question
-  let earthquakeName = data.answere
-  ellipse(width / 2, height / 2, earthquakeMag * 10, earthquakeMag * 10);
-  textAlign(CENTER);
-  text(earthquakeName, 0, height - 30, width, 30);
+  createCanvas(710, 400);
+  button = createButton(data.answere);
 }
