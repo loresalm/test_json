5a770546a7cd22930b573910294d0c84beb0617c
lorenzosalmina <lorenzo.salmina@epfl.ch>
2020-06-05 20:36:40 +0200

    updated

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 // Examples use USGS Earthquake API:
 //   https://earthquake.usgs.gov/fdsnws/event/1/#methods
-let earthquakes;
+let data;
 function preload() {
   // Get the most recent earthquake in the database
   
-  earthquakes = loadJSON('data.json');
+  data = loadJSON('data.json');
 }
 
 function setup() {
@@ -14,8 +14,8 @@ function setup() {
 function draw() {
   background(200);
   // Get the magnitude and name of the earthquake out of the loaded JSON
-  let earthquakeMag = earthquakes.features[0].properties.mag;
-  let earthquakeName = earthquakes.features[0].properties.place;
+  let earthquakeMag = data.question
+  let earthquakeName = data.answere
   ellipse(width / 2, height / 2, earthquakeMag * 10, earthquakeMag * 10);
   textAlign(CENTER);
   text(earthquakeName, 0, height - 30, width, 30);
