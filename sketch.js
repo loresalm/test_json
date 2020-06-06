5a94e7a2bea461f11da9a9be8b8aa24c4924968b
lorenzosalmina <lorenzo.salmina@epfl.ch>
2020-06-06 10:12:30 +0200

    skeleton

--- sketch.js
+++ sketch.js
@@ -4,10 +4,17 @@ let data;
 function preload() {
   // Get the most recent earthquake in the database
   
-  data = loadJSON('data.json');
+  correct = loadJSON('correct.json');
+  wrong = loadJSON('wrong.json');
 }
 
 function setup() {
-  createCanvas(710, 400);
-  button = createButton(data.answere);
+  createCanvas(800, 800);
+  fill(0);
+  textAlign(CENTER);
+  text(wrong.question, 10, 80);
+  button_a = createButton(wrong.answere[0]);
+  button_b = createButton(wrong.answere[0]);
+  button-c = createButton(wrong.answere[0]);
+
 }
